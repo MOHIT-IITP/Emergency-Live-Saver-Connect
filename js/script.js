47e9650decd77996166bc3c69283efc0d44a5c8d
MOHIT-IITP <codingbymohit@gmail.com>
2024-04-17 23:53:32 +0530

    update

---
+++ js/script.js
@@ -0,0 +1,79 @@
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+lenis.on('scroll', ScrollTrigger.update)
+
+gsap.ticker.add((time)=>{
+  lenis.raf(time * 1000)
+})
+
+gsap.ticker.lagSmoothing(0)
+
+gsap.registerPlugin(ScrollTrigger);
+
+var tl = gsap.timeline({scrollTrigger:{
+  trigger: ".page2",
+  scrub:true, 
+  start: "-30% 50%",
+  end: "50% 50%",
+  // markers: true,
+}})
+
+
+tl.to(".page1right img",{
+  scaleX:"-1",
+  top:"105%",
+  left:"5%",
+},"page1");
+
+tl.to(".circle",{
+  top:"120%",
+  left:"10%",
+  rotate:"360deg",
+},"page1");
+
+var tl2 = gsap.timeline({scrollTrigger:{
+  trigger: ".page3",
+  scrub:true, 
+  start: "-30% 50%",
+  end: "50% 50%",
+  // markers: true,
+}})
+
+tl2.to(".page1right img",{
+  transform:"none",
+  // scaleX:"-1",
+  top:"210%",
+  left:"50%",
+},"page2");
+
+tl2.to(".circle",{
+  top:"225%",
+  left:"55%",
+  rotate:"0deg",
+},"page2");
+
+var tl3 = gsap.timeline({scrollTrigger:{
+  trigger:".page4",
+  scrub:true,
+  start:"-30%, 50%",
+  end:"50%, 50%",
+  // markers:true,
+}})
+
+tl3.to(".page1right img",{
+  scaleX:"-1",
+  top:"310%",
+  left:"7%",
+},"page3");
+
+tl3.to(".circle",{
+  top:"325%",
+  left:"10%",
+  rotate:"360deg",
+},"page3");
+
+
